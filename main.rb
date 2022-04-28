efcdb553fc67519b0d994d0912e9a8272ba697f5
tkmsaaaam <59013192+tkmsaaaam@users.noreply.github.com>
2022-04-28 20:50:06 +0900

    get only 3days before

--- main.rb
+++ main.rb
@@ -12,13 +12,13 @@ err_count = 0
 
 begin
   channels.each do |c|
-    res = client.conversations_history(channel: c.id, has_more: true).messages
+    res = client.conversations_history(channel: c.id, limit: 100, latest: (Time.now - (60*60*24*3)).to_i).messages
     count = 0
     until res.size.zero? do
       res.each do |a|
-        puts client.chat_delete(channel: c.id, ts: a.ts) if a.ts.to_i < (Time.now - (60*60*24*3)).to_i
+        puts client.chat_delete(channel: c.id, ts: a.ts)
       end
-      res = client.conversations_history(channel: c.id, has_more: true).messages
+      res = client.conversations_history(channel: c.id, limit: 100, latest: (Time.now - (60*60*24*3)).to_i).messages
     end
     puts "#{c.name}:#{count}"
   end
