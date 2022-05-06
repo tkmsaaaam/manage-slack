273ef227291bdfac4595036ce2e64563691273e7
tkmsaaaam <59013192+tkmsaaaam@users.noreply.github.com>
2022-05-06 09:23:45 +0900

    fix logic

--- main.rb
+++ main.rb
@@ -10,22 +10,24 @@ channels = client.conversations_list().channels
 
 err_count = 0
 
-begin
-  channels.each do |c|
-    res = client.conversations_history(channel: c.id, limit: 100, latest: (Time.now - (60*60*24*3)).to_i).messages
-    count = 0
-    until res.size.zero? do
-      res.each do |a|
+channels.each do |c|
+  res = client.conversations_history(channel: c.id, limit: 100, latest: (Time.now - (60*60*24*3)).to_i).messages
+  count = 0
+  until res.size.zero? do
+    res.each do |a|
+      begin
         puts client.chat_delete(channel: c.id, ts: a.ts)
         count += 1
+      rescue => e
+        puts "Error #{e}"
+        sleep(1)
+        err_count += 1
+        retry if err_count < 50
       end
-      res = client.conversations_history(channel: c.id, limit: 100, latest: (Time.now - (60*60*24*3)).to_i).messages
     end
-    puts "#{c.id}:#{count}"
+    res = client.conversations_history(channel: c.id, limit: 100, latest: (Time.now - (60*60*24*3)).to_i).messages
   end
-rescue => e
-  puts "Error #{e}"
-  sleep(1)
-  err_count += 1
-  retry if err_count < 50
+  puts "#{c.id}:#{count}"
 end
+
+puts "Error occurred #{err_count} times."
