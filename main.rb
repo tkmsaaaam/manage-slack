0ef3f7b94171757fbfbb030efdb552f512198670
tkmsaaaam <59013192+tkmsaaaam@users.noreply.github.com>
2022-06-22 08:59:14 +0900

    remove counts

--- main.rb
+++ main.rb
@@ -9,28 +9,20 @@ client = Slack::Web::Client.new
 
 channels = client.conversations_list().channels
 
-err_count = 0
-total_count = 0
 THREE_DAYS_BEFORE = 60*60*24*3
 
 channels.each do |c|
-  res = client.conversations_history(channel: c.id, limit: 100, latest: (Time.now - THREE_DAYS_BEFORE).to_i).messages
-  count = 0
+  res = client.conversations_history(channel: c.id, lgimit: 100, latest: (Time.now - THREE_DAYS_BEFORE).to_i).messages
   until res.size.zero? do
     Parallel.each(res, in_processes: 20) do |r|
       begin
         puts client.chat_delete(channel: c.id, ts: r.ts)
-        count += 1
       rescue => e
         puts "Error #{e}"
         sleep(1)
-        err_count += 1
       end
     end
     res = client.conversations_history(channel: c.id, limit: 100, latest: (Time.now - THREE_DAYS_BEFORE).to_i).messages
   end
-  total_count += count
-  puts "#{c.id}:#{count}"
+  puts c.id
 end
-
-puts "Deleted #{total_count} messages.\nError occurred #{err_count} times."
