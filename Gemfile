f7fd6f1629c89d66126653e32094342cbef0e38e
tkmsaaaam <59013192+tkmsaaaam@users.noreply.github.com>
2022-06-24 08:43:33 +0900

    run bundle exec rubocop

--- Gemfile
+++ Gemfile
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 source 'http://rubygems.org'
 
+gem 'parallel'
 gem 'rubocop'
 gem 'slack-ruby-client'
-gem 'parallel'
--- main.rb
+++ main.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'slack-ruby-client'
 require 'parallel'
 
@@ -7,20 +9,18 @@ end
 
 client = Slack::Web::Client.new
 
-channels = client.conversations_list().channels
+channels = client.conversations_list.channels
 
-THREE_DAYS_BEFORE = 60*60*24*3
+THREE_DAYS_BEFORE = 60 * 60 * 24 * 3
 
 channels.each do |c|
   res = client.conversations_history(channel: c.id, lgimit: 100, latest: (Time.now - THREE_DAYS_BEFORE).to_i).messages
-  until res.size.zero? do
+  until res.size.zero?
     Parallel.each(res, in_processes: 20) do |r|
-      begin
-        puts client.chat_delete(channel: c.id, ts: r.ts)
-      rescue => e
-        puts "Error #{e}"
-        sleep(1)
-      end
+      puts client.chat_delete(channel: c.id, ts: r.ts)
+    rescue StandardError => e
+      puts "Error #{e}"
+      sleep(1)
     end
     res = client.conversations_history(channel: c.id, limit: 100, latest: (Time.now - THREE_DAYS_BEFORE).to_i).messages
   end
