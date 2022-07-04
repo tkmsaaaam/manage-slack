889b85469b6612a3c1e5d2bb57164e356667a970
tkmsaaaam <59013192+tkmsaaaam@users.noreply.github.com>
2022-07-04 21:43:55 +0900

    use START_TIME

--- main.rb
+++ main.rb
@@ -15,7 +15,7 @@ channels = client.conversations_list.channels
 THREE_DAYS_BEFORE = 60 * 60 * 24 * 3
 
 channels.each do |c|
-  res = client.conversations_history(channel: c.id, lgimit: 100, latest: (Time.now - THREE_DAYS_BEFORE).to_i).messages
+  res = client.conversations_history(channel: c.id, lgimit: 100, latest: (START_TIME - THREE_DAYS_BEFORE).to_i).messages
   until res.size.zero?
     Parallel.each(res, in_processes: 20) do |r|
       puts client.chat_delete(channel: c.id, ts: r.ts)
@@ -23,7 +23,9 @@ channels.each do |c|
       puts "Error #{e}"
       sleep(1)
     end
-    res = client.conversations_history(channel: c.id, limit: 100, latest: (Time.now - THREE_DAYS_BEFORE).to_i).messages
+    res = client.conversations_history(
+      channel: c.id, limit: 100, latest: (START_TIME - THREE_DAYS_BEFORE).to_i
+    ).messages
   end
   puts c.id
 end
