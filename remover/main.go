044b7f1b0f2922a72dc3729f8e89e344cfede672
tkmsaaaam <59013192+tkmsaaaam@users.noreply.github.com>
2025-03-13 21:48:30 +0900

    refactor

--- remover/main.go
+++ remover/main.go
@@ -25,7 +25,7 @@ func (client SlackClient) getChannels() ([]slack.Channel, error) {
 func (client SlackClient) postStartMessage() string {
 	_, ts, err := client.PostMessage(os.Getenv("SLACK_CHANNEL_ID"), slack.MsgOptionText("タスク実行を開始します", true))
 	if err != nil {
-		log.Printf("Can not post start message: %v\n", err)
+		log.Println("Can not post start message:", err)
 	}
 	return ts
 }
@@ -36,14 +36,14 @@ func (client SlackClient) postEndMessage(start time.Time, ts string, messageCoun
 	message := "タスク実行を終了します\n" + duration.String() + "\n" + "message count: " + strconv.FormatInt(int64(messageCount), 10) + "\n" + "avg: " + strconv.FormatFloat(avg, 'f', -1, 64) + "/s" + "\n" + "file count: " + strconv.FormatInt(int64(fileCount), 10)
 	_, _, err := client.PostMessage(os.Getenv("SLACK_CHANNEL_ID"), slack.MsgOptionText(message, true), slack.MsgOptionTS(ts), slack.MsgOptionBroadcast())
 	if err != nil {
-		log.Printf("End message can not post: %v\n", err)
+		log.Println("End message can not post:", err)
 	}
 }
 
 func (client SlackClient) deleteMessage(id string, ts string) {
 	_, _, err := client.DeleteMessage(id, ts)
 	if err != nil {
-		log.Printf("Can not delete message: %s : %s : %v\n", id, ts, err)
+		log.Println("Can not delete message:", id, ":", ts, ":", err)
 		if err.Error() != "message_not_found" {
 			recover()
 		}
@@ -53,7 +53,7 @@ func (client SlackClient) deleteMessage(id string, ts string) {
 func makeDays(daysStr string) int {
 	days, err := strconv.Atoi(daysStr)
 	if err != nil {
-		log.Printf("env DAYS is invalid: %v\n", err)
+		log.Println("env DAYS is invalid:", err)
 		const DAFAULT_DAYS = 3
 		return DAFAULT_DAYS
 	}
@@ -68,7 +68,7 @@ func (client SlackClient) loopInAllChannels(channels []slack.Channel, now time.T
 		params := slack.GetConversationHistoryParameters{ChannelID: id, Limit: 1000, Latest: latest}
 		res, err := client.GetConversationHistory(&params)
 		if err != nil {
-			log.Printf("Can not get history: %v\n", err)
+			log.Println("Can not get history:", err)
 			continue
 		}
 		for _, message := range res.Messages {
@@ -80,7 +80,7 @@ func (client SlackClient) loopInAllChannels(channels []slack.Channel, now time.T
 				repliesParams := slack.GetConversationRepliesParameters{ChannelID: id, Timestamp: message.Msg.Timestamp}
 				replies, _, _, err := client.GetConversationReplies(&repliesParams)
 				if err != nil {
-					log.Printf("Can not get replies: %v\n", err)
+					log.Println("Can not get replies:", err)
 				}
 				for _, reply := range replies {
 					count++
@@ -99,14 +99,14 @@ func (client SlackClient) deleteFiles(now time.Time, days int) int {
 	res, _, err := client.GetFiles(params)
 	count := 0
 	if err != nil {
-		log.Printf("Can not get file: %v\n", err)
+		log.Println("Can not get file:", err)
 		return count
 	}
 	for _, file := range res {
 		id := file.ID
 		err := client.DeleteFile(id)
 		if err != nil {
-			log.Printf("Can not delete file: %v\n", err)
+			log.Println("Can not delete file:", err)
 			continue
 		}
 		count++
@@ -115,18 +115,18 @@ func (client SlackClient) deleteFiles(now time.Time, days int) int {
 }
 
 func main() {
-	botClient := slack.New(os.Getenv("SLACK_BOT_TOKEN"))
-	userClient := slack.New(os.Getenv("SLACK_USER_TOKEN"))
+	botClient := SlackClient{slack.New(os.Getenv("SLACK_BOT_TOKEN"))}
+	userClient := SlackClient{slack.New(os.Getenv("SLACK_USER_TOKEN"))}
 	start := time.Now()
-	ts := SlackClient{botClient}.postStartMessage()
-	channels, err := SlackClient{userClient}.getChannels()
+	ts := botClient.postStartMessage()
+	channels, err := userClient.getChannels()
 	if err != nil {
-		log.Printf("%v", err)
+		log.Println("Can not get channels", err)
 		return
 	}
 	daysStr := os.Getenv("DAYS")
 	days := makeDays(daysStr)
-	messageCount := SlackClient{userClient}.loopInAllChannels(channels, start, days)
-	fileCount := SlackClient{botClient}.deleteFiles(start, days)
-	SlackClient{botClient}.postEndMessage(start, ts, messageCount, fileCount)
+	messageCount := userClient.loopInAllChannels(channels, start, days)
+	fileCount := botClient.deleteFiles(start, days)
+	botClient.postEndMessage(start, ts, messageCount, fileCount)
 }
